cce2b32f0dfd9b40fe0a5225dadfe0893b771ae1
ZakirovRifkat <75612166+ZakirovRifkat@users.noreply.github.com>
2021-04-01 02:20:36 +0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int sravn=0, perest = 0;
+
+void quicksort(int *arr,int first,int last)
+{
+    int t;
+    if (first>=last)
+        return;
+    int p_ind = first + abs(last-first) / 2;
+    int piv = arr[p_ind];
+    printf("опорный элемент: %d\n", piv);
+
+    int i = first, j = last;
+
+    do
+    {
+        while (i != p_ind && arr[i] < piv)
+        {
+            if (i < j)
+                sravn++;
+            i++;
+
+        }
+        if (i != p_ind && i < j)
+            sravn++;
+
+
+        while (j != p_ind && arr[j] > piv)
+        {
+            if (i < j)
+                sravn++;
+
+            j--;
+
+        }
+        if (j != p_ind && i < j)
+            sravn++;
+
+        if (i <= j)
+        {
+
+            if (i != j)
+            {
+                t=arr[j];
+                arr[j]=arr[i];
+                arr[i]=t;
+                perest++;
+            }
+
+            i++;
+            j--;
+
+        }
+    }
+    while (i <= j);
+
+    if (i < last)
+
+        quicksort(arr,i,last);
+
+    if (j > first)
+
+        quicksort(arr,first,j);
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+
+        int  *arr, n, i;
+        printf("Кол-во элементов в массиве:\n");
+        scanf("%d", &n);
+        int first = 0, last = n-1;
+        arr = (int*) malloc(n*sizeof(int));
+        printf("Элементы массива\n");
+        for (i=0; i<n; i++)
+            scanf("%d", &arr[i]);
+        quicksort(arr, first, last) ;
+        printf("\nОтсортированный массив:\n");
+        for(i=0; i<n; i++)
+            printf("%d  ", arr[i]);
+        printf("\nКоличество перестановок: ");
+        printf("%d\n",perest);
+        printf("Количество сравнений: ");
+        printf("%d\n", sravn);
+        sravn = 0;
+        perest = 0;
+
+
+
+}
